ba8d6137e0dad4411691331c40d6ff972c5b217e
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-25 05:19:55 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -5,6 +5,14 @@ class User
     @first_name = first_name
     @last_name = last_name
   end
+  def first_name=(name)
+    @first_name = name
+  end
+  def last_name=(name)
+    @last_name = name
+  end
+  
+  
   def first_name
     @first_name
   end
